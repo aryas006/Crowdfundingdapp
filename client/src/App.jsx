a9bf243a9345c165919f93b66feec46b7582abaa
Arya <128399498+aryas006@users.noreply.github.com>
2023-11-18 23:19:00 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import { CampaignDetails, CreateCompaign, Home, Profile } from "./pages";
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="relative text-white sm:=8 p-4 bg-[black] min-h-screen flex flex-row">
+      <div className="sm:flex hiddent mr-10 relative">Sidebar</div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        Navbar
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </h1>
+  );
 }
---
+++ client/src/pages/CreateCompaign.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateCompaign = () => {
+  return <div>CreateCompaign</div>;
+};
+
+export default CreateCompaign;
