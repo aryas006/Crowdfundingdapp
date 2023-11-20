352d72132ce63da458fe27a6c3dd6e7b0387446e
Arya <128399498+aryas006@users.noreply.github.com>
2023-11-20 10:08:34 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./index.css";
 import { Route, Routes } from "react-router-dom";
+import { Navbar, Sidebar } from "./components";
 import { CampaignDetails, CreateCompaign, Home, Profile } from "./pages";
 export default function App() {
   return (
-    <h1 className="relative text-white sm:=8 p-4 bg-[black] min-h-screen flex flex-row">
-      <div className="sm:flex hiddent mr-10 relative">Sidebar</div>
+    <h1 className="relative text-white sm:-8 p-4 bg-[black] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
